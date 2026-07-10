6ed396ad5480a890c78b76026e07b59e6b79dfbe
Vishesh Pandey <behappy9699@gmail.com>
2026-07-10 22:40:36 +0530

    Time: 1 ms (31.11%), Space: 42.4 MB (16.01%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.java
+++ 0868-binary-gap/0868-binary-gap.java
@@ -3,11 +3,11 @@ class Solution {
         int d=0;
         int max=0;
         boolean flag=false;
-        StringBuilder s=new StringBuilder();
-        while(n>0){
-            s.append(n%2);
-            n/=2;
-        }
+        String s=Integer.toBinaryString(n);
+        // while(n>0){
+        //     s.append(n%2);
+        //     n/=2;
+        // }
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='1'&&flag){
                 max=Math.max(max,i-d);
